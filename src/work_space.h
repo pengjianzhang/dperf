801be1870f13d1a9b252292e1a881adcf207d766
Jianzhang Peng <pengjianzhang@gmail.com>
2022-06-20 14:07:17 +0000

    test

--- src/work_space.h
+++ src/work_space.h
@@ -179,7 +179,7 @@ static inline void work_space_tx_send_tcp(struct work_space *ws, struct rte_mbuf
     work_space_tx_send(ws, mbuf);
 }
 
-#define SEND_ONCE   8
+#define SEND_ONCE   4
 static inline void work_space_tx_send_udp(struct work_space *ws, struct rte_mbuf *mbuf)
 {
     int i = 0;
